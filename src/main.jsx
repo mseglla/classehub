196d69ab6cd00dace71eb2f362cb3b5dd748fb04
Marc Segura <marcsegurallado@gmail.com>
2026-06-03 23:50:55 +0200

    Enable Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,11 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./App.css";
+import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+    <>
+      <App />
+      <Analytics />
+    </>
+  );
